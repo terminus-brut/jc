0f68c4a3e0373bdec62af83453451f542682c847
Marian Koncek <mkoncek@redhat.com>
2021-07-20 14:28:02 +0200

    Add license headers

--- src/main/java/io/github/mkoncek/classpathless/api/ClassesProvider.java
+++ src/main/java/io/github/mkoncek/classpathless/api/ClassesProvider.java
@@ -1,3 +1,18 @@
+/*-
+ * Copyright (c) 2021 Marián Konček
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.github.mkoncek.classpathless.api;
 
 import java.util.Collection;
@@ -13,7 +28,7 @@ public interface ClassesProvider {
     Collection<IdentifiedBytecode> getClass(ClassIdentifier... names);
 
     /**
-     * Warning, may include lambdas and will include inner classes with $notations.
+     * Warning: may include lambdas and will include inner classes with $notations.
      * Intentionally not using ClassIdentifier, but may change to it
      *
      * @return all fully qualified classes visible from classpath
--- src/main/java/io/github/mkoncek/classpathless/api/IdentifiedBytecode.java
+++ src/main/java/io/github/mkoncek/classpathless/api/IdentifiedBytecode.java
@@ -1,13 +1,27 @@
+/*-
+ * Copyright (c) 2021 Marián Konček
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.github.mkoncek.classpathless.api;
 
 /**
- * Compiled bytecode of class with its fully qualified name
+ * Compiled bytecode of a class with its fully qualified name.
  */
 public class IdentifiedBytecode extends IdentifiedFile {
     /**
-     *
-     * @param classIdentifier fully qualified name of class
-     * @param file bytecode content of the file
+     * @param classIdentifier Fully qualified name of class.
+     * @param file The bytecode content of the file.
      */
     public IdentifiedBytecode(ClassIdentifier classIdentifier, byte[] file) {
         super(classIdentifier, file);
